333a3fd598c0362641f4258ccccacd02105d87d7
mathe1sOutlook <118584086+mathe1sOutlook@users.noreply.github.com>
2025-08-26 23:40:58 -0300

    Add Firebase configuration

---
+++ onevision/hosting/assets/js/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDuWN3PVSpEJ1NRPg2Y94qCfCp1KFzB9a0",
+  authDomain: "visionone-9c4b9.firebaseapp.com",
+  projectId: "visionone-9c4b9",
+  storageBucket: "visionone-9c4b9.firebasestorage.app",
+  messagingSenderId: "62737604021",
+  appId: "1:62737604021:web:5efe8bd142e77d7288cdfc",
+  measurementId: "G-B3EVP6GSZY",
+  databaseURL: "https://visionone-9c4b9-default-rtdb.firebaseio.com"
+};
